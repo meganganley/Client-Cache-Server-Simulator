897be533f278f845fbd7c55071c9048028cdbdc3
Megan Ganley <mgn.gnly@gmail.com>
2016-08-09 14:54:18 +1200

    Init cache -- not connected or functional yet

--- Cache/Cache.ConsoleApp/Program.cs
+++ Cache/Cache.ConsoleApp/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using Server.Service;
+using System.Text;
+using System.Threading.Tasks;
+using Cache.Service;
 
-namespace Server.ConsoleApp
+namespace Cache.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Step 1 Create a URI to serve as the base address.
-            Uri baseAddress = new Uri("http://localhost:8081/Server/");
+            Uri baseAddress = new Uri("http://localhost:8081/Server/");     //TODO 
 
             // Step 2 Create a ServiceHost instance
-            ServiceHost selfHost = new ServiceHost(typeof(Service.FileService), baseAddress);
+            ServiceHost selfHost = new ServiceHost(typeof(CacheFileService), baseAddress);
 
             try
             {
                 // Step 3 Add a service endpoint.
-                selfHost.AddServiceEndpoint(typeof(IFileService), new WSHttpBinding(),  "Service");// change from ws to basic
+                selfHost.AddServiceEndpoint(typeof(ICacheFileService), new WSHttpBinding(), "Service");// change from ws to basic
 
                 // Step 4 Enable metadata exchange.
                 ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
---
+++ Cache/Cache.Service/CacheFileService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Cache.Service
+{
+    public class CacheFileService : ICacheFileService
+    {
+        public string GetFileNames(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
